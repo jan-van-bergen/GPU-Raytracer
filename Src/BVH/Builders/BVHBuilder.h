2bf24ae8a9c2e54d4413afde9276285ee8d06707
jan <janvanbergen16@gmail.com>
2021-06-28 15:05:59 +0200

    Fixed precision issues with center calculations during BVH construction

--- Src/BVH/Builders/BVHBuilder.h
+++ Src/BVH/Builders/BVHBuilder.h
@@ -20,7 +20,7 @@ private:
 	int max_primitives_in_leaf;
 
 	template<typename Primitive>
-	void build_bvh_recursive(BVHNode & node, const Primitive * primitives, int * indices[3], int & node_index, int first_index, int index_count) {
+	void build_bvh_recursive(BVHNode & node, const Primitive * primitives, const Vector3 * centers, int * indices[3], int & node_index, int first_index, int index_count) {
 		node.aabb = BVHPartitions::calculate_bounds(primitives, indices[0], first_index, first_index + index_count);
 		
 		if (index_count == 1) {
@@ -53,7 +53,7 @@ private:
 		node.left = node_index;
 		node_index += 2;
 		
-		float split = primitives[indices[split_dimension][split_index]].get_center()[split_dimension];
+		float split = centers[indices[split_dimension][split_index]][split_dimension];
 		BVHPartitions::split_indices(primitives, indices, first_index, index_count, temp, split_dimension, split_index, split);
 
 		node.count = (split_dimension + 1) << 30;
@@ -61,25 +61,33 @@ private:
 		int num_left  = split_index - first_index;
 		int num_right = first_index + index_count - split_index;
 
-		build_bvh_recursive(bvh->nodes[node.left    ], primitives, indices, node_index, first_index,            num_left);
-		build_bvh_recursive(bvh->nodes[node.left + 1], primitives, indices, node_index, first_index + num_left, num_right);
+		build_bvh_recursive(bvh->nodes[node.left    ], primitives, centers, indices, node_index, first_index,            num_left);
+		build_bvh_recursive(bvh->nodes[node.left + 1], primitives, centers, indices, node_index, first_index + num_left, num_right);
 	}
 
 	template<typename Primitive>
 	inline void build_bvh_impl(const Primitive * primitives, int primitive_count) {
-		std::sort(indices_x, indices_x + primitive_count, [&](int a, int b) { return primitives[a].get_center().x < primitives[b].get_center().x; });
-		std::sort(indices_y, indices_y + primitive_count, [&](int a, int b) { return primitives[a].get_center().y < primitives[b].get_center().y; });
-		std::sort(indices_z, indices_z + primitive_count, [&](int a, int b) { return primitives[a].get_center().z < primitives[b].get_center().z; });
+		Vector3 * centers = new Vector3[primitive_count];
+
+		for (int i = 0; i < primitive_count; i++) {
+			centers[i] = primitives[i].get_center();
+		}
+
+		std::sort(indices_x, indices_x + primitive_count, [centers](int a, int b) { return centers[a].x < centers[b].x; });
+		std::sort(indices_y, indices_y + primitive_count, [centers](int a, int b) { return centers[a].y < centers[b].y; });
+		std::sort(indices_z, indices_z + primitive_count, [centers](int a, int b) { return centers[a].z < centers[b].z; });
 		
 		int * indices[3] = { indices_x, indices_y, indices_z };
 	
 		int node_index = 2;
-		build_bvh_recursive(bvh->nodes[0], primitives, indices, node_index, 0, primitive_count);
+		build_bvh_recursive(bvh->nodes[0], primitives, centers, indices, node_index, 0, primitive_count);
 
 		assert(node_index <= 2 * primitive_count);
 
 		bvh->node_count  = node_index;
 		bvh->index_count = primitive_count;
+
+		delete [] centers;
 	}
 
 public:
