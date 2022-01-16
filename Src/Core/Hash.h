7081eaad74f580de79e4125753b42cd77ca47438
jan <janvanbergen16@gmail.com>
2022-01-16 15:53:35 +0100

    Don't use MSVC specific ui64 literal suffix

--- Src/Core/Hash.h
+++ Src/Core/Hash.h
@@ -4,8 +4,8 @@
 namespace FNVHash {
 	inline size_t hash(const char bytes[], size_t length) {
 		// Based on: https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
-		constexpr size_t FNV_OFFSET_BASIS = 14695981039346656037ui64;
-		constexpr size_t FNV_PRIME        = 1099511628211ui64;
+		constexpr size_t FNV_OFFSET_BASIS = 14695981039346656037ull;
+		constexpr size_t FNV_PRIME        = 1099511628211ull;
 
 		size_t hash = FNV_OFFSET_BASIS;
 		for (size_t i = 0; i < sizeof(bytes); i++) {
