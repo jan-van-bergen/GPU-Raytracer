a958732abbaaa43fdeef6ec7d282f0c98aa9a0a9
jan <janvanbergen16@gmail.com>
2022-06-11 14:20:41 +0200

    Cleanup: Remove outdated files

--- Src/Util/Multiscatter.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace Multiscatter {
-	extern const float E_ms_enter[16 * 16 * 16];
-	extern const float E_ms_leave[16 * 16 * 16];
-
-	extern const float E_avg_enter[16 * 16];
-	extern const float E_avg_leave[16 * 16];
-}
