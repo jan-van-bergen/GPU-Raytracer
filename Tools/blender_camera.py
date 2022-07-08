0964964669c683fe29e9c939140c3f9ea8c50dbf
jan <janvanbergen16@gmail.com>
2022-07-08 19:42:09 +0200

    Blender camera export script

---
+++ Tools/blender_camera.py
@@ -0,0 +1,20 @@
+"""
+Blender Script to export hair camera to mitsuba XML
+"""
+
+import bpy
+import math
+import mathutils
+
+camera = bpy.context.scene.camera
+matrix = mathutils.Matrix.Rotation(math.radians(-90.0), 4, 'X') @ camera.matrix_world
+
+with open('camera.xml', 'w') as file:
+    file.write(f'<transform name="toWorld">\n')
+    file.write(f'\t<matrix value="\n')
+    file.write(f'\t\t{matrix[0][0]} {matrix[0][1]} {-matrix[0][2]} {matrix[0][3]}\n')
+    file.write(f'\t\t{matrix[1][0]} {matrix[1][1]} {-matrix[1][2]} {matrix[1][3]}\n')
+    file.write(f'\t\t{matrix[2][0]} {matrix[2][1]} {-matrix[2][2]} {matrix[2][3]}\n')
+    file.write(f'\t\t{matrix[3][0]} {matrix[3][1]} {-matrix[3][2]} {matrix[3][3]}\n')
+    file.write(f'\t"/>\n')
+    file.write(f'</transform>\n')
--- Tools/blender_mitshair.py
+++ Tools/blender_mitshair.py
@@ -6,20 +6,17 @@ import bpy
 
 me = bpy.context.object.data
 
-file = open("hair.mitshair", "w")
+with open("hair.mitshair", "w") as file:
+	prev_index_b = -1
 
-prev_index_b = -1
+	for edge in me.edges:
+		index_a, index_b = edge.vertices
 
-for edge in me.edges:
-    index_a, index_b = edge.vertices
+		if (prev_index_b != -1 and index_a != prev_index_b):
+			coord = me.vertices[prev_index_b].co
+			file.write(f"{coord[0]} {coord[2]} {-coord[1]}\n\n")
 
-    if (prev_index_b != -1 and index_a != prev_index_b):
-        coord = me.vertices[prev_index_b].co
-        file.write(f"{coord[0]} {coord[2]} {-coord[1]}\n\n")
+		coord = me.vertices[index_a].co
+		file.write(f"{coord[0]} {coord[2]} {-coord[1]}\n")
 
-    coord = me.vertices[index_a].co
-    file.write(f"{coord[0]} {coord[2]} {-coord[1]}\n")
-
-    prev_index_b = index_b
-
-file.close()
+		prev_index_b = index_b
